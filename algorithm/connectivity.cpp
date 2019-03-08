a8ec2f62cc5c78b1f29dda62ce532fe60d3c1e09
liweifdu <lweifdu@gmail.com>
2019-03-08 21:04:11 +0800

    add fastmerge algorithm

--- algorithm/connectivity.cpp
+++ algorithm/connectivity.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 static const int N = 1000;
-int main(){
+int fastsearch(){
     int i, p, q, id[N];
     for (i = 0; i < N; ++i)
         id[i] = i;
@@ -21,4 +21,28 @@ int main(){
         }
         cout << " " << p << " " << q << endl;
     }
+    return 0;
+}
+
+int fastmerge(){
+    int i, j, p, q, id[N];
+    for (i = 0; i < N; ++i)
+        id[i] = i;
+    while (cin >> p  >> q){
+        for (i = p; i != id[i]; i = id[i]);
+        for (j = q; j != id[j]; j = id[j]);
+        if (i == j){
+            cout << "the two numbers are connected" << endl;
+            cout << id[p] << id[q] << endl;
+            continue;
+        }
+        id[i] = j;
+        cout << " " << p << " " << q << endl;
+    }
+    return 0;
+}
+
+int main(){
+    //fastsearch();
+    fastmerge();
 }
