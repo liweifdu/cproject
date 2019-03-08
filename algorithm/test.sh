b989698dcc9760d41da3efec36d6c4015e938bf4
liweifdu <lweifdu@gmail.com>
2019-03-08 16:24:23 +0800

    modify the shell

--- algorithm/test.sh
+++ algorithm/test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+#rmove the all project files
 rm *.o
 
-g++ -g -std=c++11 $1 -o "$1.o"
-./$1.o
+#parameter
+CPP_NAME="$1"
+SRC_FILE="$CPP_NAME.cpp"
+OUT_FILE="$CPP_NAME.o"
+
+
+#command
+g++ -g -std=c++11 -Wall -Wextra -pedantic $SRC_FILE -o $OUT_FILE
+./$OUT_FILE
