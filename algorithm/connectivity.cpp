070e957b6d4f9ee38f92e8105af552a3f8250521
liweifdu <lweifdu@gmail.com>
2019-03-11 16:13:01 +0800

    the final version of connectivity problem

--- algorithm/connectivity.cpp
+++ algorithm/connectivity.cpp
@@ -67,8 +67,35 @@ int fastmergeweight(){
     return 0;
 }
 
+int equaldivide(){
+    int i, j, p, q, id[N], sz[N];
+    for (i = 0; i < N; i++){
+        id[i] = i;
+        sz[i] = i;
+    }
+    while (cin >> p >> q){
+        for (i = p; i != id[i]; i = id[i])
+            id[i] = id[id[i]];
+        for (j = q; j != id[j]; j = id[j])
+            id[j] = id[id[j]];
+        if (i == j)
+            continue;
+        if (sz[i] < sz[j]){
+            id[i] = j;
+            sz[j] += sz[i];
+        }
+        else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+        cout << " " << p << " " << q << endl;
+    }
+    return 0;
+}
+
 int main(){
     //fastsearch();
     //fastmerge();
-    fastmergeweight();
+    //fastmergeweight();
+    equaldivide();
 }
