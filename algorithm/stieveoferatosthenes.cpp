3a7a243373f931ea6a5f9ab28825e68bf51a7d44
liweifdu <lweifdu@gmail.com>
2019-03-15 14:49:58 +0800

    add the stieve algorithm

---
+++ algorithm/stieveoferatosthenes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+
+static const int N = 1000;
+using namespace std;
+
+int main(){
+    int a[N];
+    memset(a, 1, sizeof(a));
+    for(int i = 2; i < N; ++i)
+        if(a[i])
+            for(int j = i; j*i < N; j++)
+                a[i*j] = 0;
+    for(int i = 2; i < N; i++)
+        if (a[i])
+            cout << " " << i;
+    cout << endl;
+}
